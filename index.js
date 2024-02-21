0e84f71dc614d638c496efe8a519ea08b16d5af3
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2024-02-21 15:51:39 -0300

    Funcção para inicializar o joog e preparar o tabuleiro.

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function upDateTitle() { //função p exibir o nome do jogador da vez
     const playerInput = document.getElementById(turnPlayer)
     document.getElementById('turnPlayer').innerText = playerInput.value
 }
+
+function initializeGame() { //função para inicializar o jogo e preparar o tabuleiro
+    vBoard = [['','',''] , ['','',''] , ['','',''] ] //para mostrar no console como está o tabuleiro
+    turnPlayer = 'player1'
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+    upDateTitle()
+    boardRegions.forEach(function (element){
+        element.classList.remove('win')
+        element.innerText = ''
+        element.addEventListener('click', handleBoardClick)
+    })
+}
